4a4dbb3fef4297050d4a13ceae7a7bd64f3d71e8
Durvaal <plimayep@gmail.com>
2022-07-04 21:09:41 -0300

    feat: add send bulk message from bluetooth mac addresses with parallel stream

--- src/main/java/integration/Main.java
+++ src/main/java/integration/Main.java
@@ -1,5 +1,6 @@
 package integration;
 
+import com.intel.bluetooth.RemoteDeviceHelper;
 import integration.bluetooth.ClientServer;
 import integration.bluetooth.ServiceName;
 import integration.bluetooth.ServicesSearch;
@@ -7,7 +8,9 @@ import integration.device.Device;
 import integration.message.Message;
 import integration.message.MessageType;
 
+import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,17 +19,39 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.sendBulkMessage(MessageType.GENERIC, ServiceName.OBEX_OBJECT_PUSH, "Oláaaaaaaa");
+        // main.sendBulkMessageFromBluetoothDevices(MessageType.GENERIC, ServiceName.OBEX_OBJECT_PUSH, "Oláaaaaaaa");
+        main.sendBulkMessageFromFixedListDevices(MessageType.GENERIC, ServiceName.OBEX_OBJECT_PUSH, "Agora sim");
     }
 
-    public void sendMessage() {
-        Message message = new Message("btgoep://38E39F6E4F37:12;authenticate=false;encrypt=false;master=false", MessageType.GENERIC, "ollaa");
+    public void sendBulkMessageFromFixedListDevices(MessageType messageType, ServiceName serviceName, String content) {
         ClientServer clientServer = new ClientServer();
+        // Moto G, OBEX_OBJECT_PUSH connection URL: btgoep://38E39F6E4F37:12;authenticate=false;encrypt=false;master=false
+        // Galaxy A10s, OBEX_OBJECT_PUSH connection URL: btgoep://78232762EE21:12;authenticate=false;encrypt=false;master=false
+        List<String> bluetoothMacAddresses = new ArrayList<String>() {
+            {
+                add("38E39F6E4F37");
+                add("78232762EE21");
+            }
+        };
+        List<Device> devicesToSendMessage = bluetoothMacAddresses.stream()
+                .map(bluetoothMacAddress -> new Device(
+                        serviceName,
+                        bluetoothMacAddress,
+                        "btgoep://" + bluetoothMacAddress + ":12;authenticate=false;encrypt=false;master=false"
+                )).collect(Collectors.toList());
 
-        clientServer.sendMessage(message);
+        System.out.println("\nInit send bulk message to " + devicesToSendMessage.size() + " devices.");
+
+        devicesToSendMessage.parallelStream().forEach(device -> {
+            System.out.println(device);
+            Message message = new Message(device.getConnectionURL(), messageType, content);
+            clientServer.sendMessage(message);
+        });
+
+        System.out.println("Finish send bulk message to devices.\n");
     }
 
-    public void sendBulkMessage(MessageType messageType, ServiceName serviceName, String content) {
+    public void sendBulkMessageFromBluetoothDevices(MessageType messageType, ServiceName serviceName, String content) {
         List<Device> devicesToSendMessage = searchDevicesServices(serviceName);
         ClientServer clientServer = new ClientServer();
 
